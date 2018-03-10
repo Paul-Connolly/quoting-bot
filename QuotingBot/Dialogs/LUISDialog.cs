b6c4626837c635ac8a1bfdaf08589cfd1fba87d2
unknown <paul.connolly@appliedsystems.com>
2018-03-10 18:16:00 +0000

    started adding LUIS dialog

---
+++ QuotingBot/Dialogs/LUISDialog.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using QuotingBot.Models;
+using System;
+
+namespace QuotingBot.Dialogs
+{
+    [LuisModel("fdc7ea4d-1480-467f-978e-61d4a3345051", "0caa7c26506c48bb801e68daca06077c")]
+    [Serializable]
+    public class LUISDialog : LuisDialog<QuoteType>
+    {
+    }
+}
